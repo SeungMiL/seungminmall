8a2172a39ab6bb1ab6031c0b19d62ebc415dbe7e
SeungMiL <tmskqj@naver.com>
2022-03-07 23:55:50 +0900

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from './pages/Home';
 
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Home />
   );
 }
 
